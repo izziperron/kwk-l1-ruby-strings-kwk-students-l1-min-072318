3709c27b99742c619caaef92a54e2ab92650b93f
izziperron <izziperron@gmail.com>
2018-07-24 02:34:50 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -8,6 +8,6 @@ puts "How much do you love them?"
 amount = gets.chomp 
 puts "Who is writing this letter?"
 author = gets.chomp 
-puts "Hello #{name}, this is a letter for you. I just want you to know that I think you are a(n) #{personality} and I appreciate all that you do. Even though you are my #{relation}, you are also my friend. I love you #{amount}."
+puts "Hello #{name}, this is a letter for you. I just want you to know that I think you are a(n) #{personality} person and I all that you do. Even though you are my #{relation}, you are also my friend. I love you #{amount}."
 
 puts "Love, #{author}"
